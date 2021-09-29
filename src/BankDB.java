6be97d4e8c0efa71ba8d81e6793f4f51f2c63aa9
Evan Galindo <evangalindo@outlook.com>
2021-09-28 20:40:41 -0700

    Added BankDB class with add record method.

---
+++ src/BankDB.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class BankDB {
+    /**Takes record of ATM and adds it to a txt file using a loop system that looks through the file and stops when i is = to the length of then writes
+     * whatever is in the variable acsessing the method in a comma delimited format
+     * 
+     * @param fileName 
+     * @param values
+     * @throws Exception
+     */
+    public void addRecord(String fileName, String[] values) throws Exception {
+        String line = new String();
+         
+        for (int i = 0; i < values.length; i++){
+            line += values[i] + ",";
+        }
+        
+        line = line.substring(0, line.length() - 1);
+        line += System.lineSeparator();
+
+        String fileAdress = System.getProperty("user.dir") + "\\src\\" + fileName;
+
+        File record = new File(fileAdress);
+        
+        FileOutputStream output = new FileOutputStream(record, true);
+        output.write(line.getBytes());
+        output.close();
+    }
+}
